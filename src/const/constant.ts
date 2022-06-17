52e1e10e016770889a05e8b442114d6d0c393c2e
AyselAmrahli <ayselamrahli@gmail.com>
2022-06-17 10:43:31 +0400

    react-create-app on Heroku

--- src/const/constant.ts
+++ src/const/constant.ts
@@ -7,7 +7,7 @@ import { IAppMenuList, IAppRoute } from "./model";
 
 export const getApiKey = () => '56b4e93d2204bee0e2d35d74719395af';
 
-const BASE_URL = 'http://api.themoviedb.org/3'
+const BASE_URL = 'https://api.themoviedb.org/3'
 
 export const getApiUrl = (url: string, query?: string) => {
   if(query) return `${BASE_URL}/search/movie?api_key=${getApiKey()}&query=${query}`;
